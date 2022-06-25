af083aee812341eef3007e4dc68695f10e6304c5
Yuan Wang <gityw2021mbp13@yuanagain.com>
2022-06-25 00:27:59 +0000

    rest of the repo

--- apps/webapp/src/app/app.tsx
+++ apps/webapp/src/app/app.tsx
@@ -1,14 +1,42 @@
 import styled from '@emotion/styled';
 import NxWelcome from './nx-welcome';
+import { ApolloExplorerReact, ApolloExplorer } from '@apollo/explorer';
 
 const StyledApp = styled.div`
   // Your style here
+  min-height: 900px;
+
+  min-width: 95vw;
+  display: flex;
+  align-items: stretch;
+  justify-content: flex-start;
+  flex-direction: column;
+  background: red;
+
 `;
 
 export function App() {
   return (
     <StyledApp>
-      <NxWelcome title="webapp" />
+
+      <ApolloExplorerReact
+
+        graphRef='spacex-yw@current'
+        endpointUrl='https://api.spacex.land/graphql/'
+        // initialState={{
+        //   document: `query Example {
+        //     me {
+        //       id
+        //     }
+        // }`,
+        //   variables: {
+        //     test: 'abcxyz',
+        //   },
+        //   displayOptions: {
+        //     showHeadersAndEnvVars: true,
+        //   },
+        // }}
+      />
     </StyledApp>
   );
 }
